010ff25f445efa4489403b3616aaf081cf4c4af0
sonu <sonu221553@gmail.com>
2018-01-13 13:02:26 +0530

    user input feature

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,7 +1,12 @@
 # Defining class
 
 class calculator:
-
+	def userInput():
+		x = raw_input("Enter first number :")
+		y = raw_input("Enter second number :")
+		return [x,y]
+		
 
 if "__name__"=="__main__":
-	calculator()
+	app = calculator()
+	app.userInput()

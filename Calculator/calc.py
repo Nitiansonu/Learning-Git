1994a2436ee5d51a73e564c0e28f91cbc1428e1c
sonu <sonu221553@gmail.com>
2018-01-13 12:00:31 +0530

    nothing change

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,7 +1,10 @@
 # Defining class
 
 class calculator:
-
+	def userInput():
+		x = raw_input("Enter first number :")
+		y = raw_input("Enter second number :")
+		return [x,y]
 		
 
 if "__name__"=="__main__":

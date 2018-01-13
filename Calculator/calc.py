c1c5682adb17c51fb0e4aa87cc4eb41959844cbd
sonu <sonu221553@gmail.com>
2018-01-13 12:03:50 +0530

    addition feature

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -5,6 +5,9 @@ class calculator:
 		x = raw_input("Enter first number :")
 		y = raw_input("Enter second number :")
 		return [x,y]
+
+	def adding():
+		print "sonu"
 		
 
 if "__name__"=="__main__":

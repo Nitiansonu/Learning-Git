2cf5fb8bd65f928467aa3f1f8febebc40767b014
sonu <sonu221553@gmail.com>
2018-01-13 11:22:16 +0530

    adding user input function in calculator class

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,7 +1,9 @@
 # Defining class
 
 class calculator:
-
+	def userInput():
+		x = raw_input("Enter first number :")
+		y = raw_input("Enter second number :")
 
 if "__name__"=="__main__":
 	calculator()

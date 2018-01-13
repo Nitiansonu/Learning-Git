d2f49fd96e1a3d74152d1ab25386f61ec6212da4
sonu <sonu221553@gmail.com>
2018-01-13 11:53:57 +0530

    modifying main

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,10 +1,7 @@
 # Defining class
 
 class calculator:
-	def userInput():
-		x = raw_input("Enter first number :")
-		y = raw_input("Enter second number :")
-		return [x,y]
+
 		
 
 if "__name__"=="__main__":

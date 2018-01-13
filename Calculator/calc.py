8665941897aafaf769449f2895bc6dacc44b3ac3
sonu <sonu221553@gmail.com>
2018-01-13 12:09:44 +0530

    cleaning master

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,14 +1,7 @@
 # Defining class
 
 class calculator:
-	def userInput():
-		x = raw_input("Enter first number :")
-		y = raw_input("Enter second number :")
-		return [x,y]
 
-	def adding():
-		print "sonu"
-		
 
 if "__name__"=="__main__":
 	app = calculator()
